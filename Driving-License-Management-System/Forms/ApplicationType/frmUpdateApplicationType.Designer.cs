ae722ec36bfa2db8e6f8b95e8cad3bd227dabf64
aimad <moustajil.dev@gmail.com>
2026-06-17 17:41:05 +0100

    Complet ui and function update at home

---
+++ Driving-License-Management-System/Forms/ApplicationType/frmUpdateApplicationType.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Driving_License_Management_System.Forms.ApplicationType
+{
+    partial class frmUpdateApplicationType
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            lbID = new Label();
+            tbTitle = new TextBox();
+            tbFees = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(115, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(276, 31);
+            label1.TabIndex = 0;
+            label1.Text = "Update Application type";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(25, 96);
+            label2.Name = "label2";
+            label2.Size = new Size(33, 20);
+            label2.TabIndex = 1;
+            label2.Text = "ID :";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(25, 152);
+            label3.Name = "label3";
+            label3.Size = new Size(48, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Title :";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(25, 208);
+            label4.Name = "label4";
+            label4.Size = new Size(48, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Fees :";
+            // 
+            // lbID
+            // 
+            lbID.AutoSize = true;
+            lbID.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbID.Location = new Point(115, 96);
+            lbID.Name = "lbID";
+            lbID.Size = new Size(23, 20);
+            lbID.TabIndex = 4;
+            lbID.Text = "??";
+            // 
+            // tbTitle
+            // 
+            tbTitle.Location = new Point(115, 145);
+            tbTitle.Name = "tbTitle";
+            tbTitle.Size = new Size(234, 27);
+            tbTitle.TabIndex = 5;
+            // 
+            // tbFees
+            // 
+            tbFees.Location = new Point(115, 201);
+            tbFees.Name = "tbFees";
+            tbFees.Size = new Size(234, 27);
+            tbFees.TabIndex = 6;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(369, 259);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 7;
+            button1.Text = "Update";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(234, 259);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 8;
+            button2.Text = "Cancel";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // frmUpdateApplicationType
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(487, 309);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(tbFees);
+            Controls.Add(tbTitle);
+            Controls.Add(lbID);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "frmUpdateApplicationType";
+            Text = "frmUpdateApplicationType";
+            Load += frmUpdateApplicationType_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label lbID;
+        private TextBox tbTitle;
+        private TextBox tbFees;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ Driving-License-Management-System/Forms/ApplicationType/frmUpdateApplicationType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Driving_License_Management_System.Forms.ApplicationType
+{
+    public partial class frmUpdateApplicationType : Form
+    {
+        public frmUpdateApplicationType()
+        {
+            InitializeComponent();
+        }
+
+        private void frmUpdateApplicationType_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
